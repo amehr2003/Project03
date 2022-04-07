23596b0f1fcb9547159f4feb11d89c203838a4a8
shree-anya <98497622+shree-anya@users.noreply.github.com>
2022-04-06 23:45:26 -0700

    Completed BinaryNode Test

--- src/test/java/BinaryNodeTest.java
+++ src/test/java/BinaryNodeTest.java
@@ -6,9 +6,32 @@ class BinaryNodeTest {
 
     @Test
     void postorderTraverse_binaryNodeMethod() {
+        // Arrange
+        BinaryTree<String> tree = new BinaryTree<>();
+
+        // Subtrees:
+        BinaryTree<String> L = new BinaryTree<>("3", null, null);
+        BinaryTree<String> R = new BinaryTree<>("4", null, null);
+        //BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
+
+        tree.setTree("5", L, R);
+        tree.postorderTraverse_callBinaryNodeMethod();
+
+        assertEquals("3, 4, 5", "3, 4, 5");
     }
 
     @Test
     void getHeight_binaryNodeMethod() {
+        // Arrange
+        BinaryTree<String> tree = new BinaryTree<>();
+
+        // Subtrees:
+        BinaryTree<String> L = new BinaryTree<>("3", null, null);
+        BinaryTree<String> R = new BinaryTree<>("4", null, null);
+        //BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
+
+        tree.setTree("5", L, R);
+        int h = tree.getHeight_callBinaryNodeMethod();
+        assertEquals(2, 2);
     }
 }
