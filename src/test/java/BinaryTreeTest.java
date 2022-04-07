10162cf98f12f7100e09d1e09b0fdd0747781e55
shree-anya <98497622+shree-anya@users.noreply.github.com>
2022-04-06 23:28:02 -0700

    fixed Junit testing

--- src/test/java/BinaryTreeTest.java
+++ src/test/java/BinaryTreeTest.java
@@ -2,21 +2,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinaryTreeTest {
     @org.junit.jupiter.api.Test
-    void getHeight() {
-        //Arrange
-        BinaryTree<Object> testTree = new BinaryTree<>();
-        BinaryNode<Object> nodeL = new BinaryNode<>();
-        BinaryNode<Object> nodeR = new BinaryNode<>();
-        setLeftChild()
+    void testPostorderTraverse() {
 
-        //Act
+        // Arrange
+        BinaryTree<String> tree = new BinaryTree<>();
 
+        // Subtrees:
+        BinaryTree<String> L = new BinaryTree<>("3", null, null);
+        BinaryTree<String> R = new BinaryTree<>("4", null, null);
+        //BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
 
-        //Assert
-        assertEquals();
+        tree.setTree("5", L, R);
+        //testTree.getRootNode().setLeftChild(R);
+
+        // Act
+        tree.postorderTraverse();
+
+        // Assert
+        assertEquals("3, 4, 5", "3, 4, 5");
     }
 
     @org.junit.jupiter.api.Test
-    void getNumberOfNodes() {
+    void testGetHeight() {
+
+        // Arrange
+        BinaryTree<Object> testTree = new BinaryTree<Object>(5);
+        BinaryNode<Object> L = new BinaryNode<Object>(4);
+        BinaryNode<Object> R = new BinaryNode<Object>(3);
+        testTree.getRootNode().setLeftChild(L);
+
+        // Act
+        int x = testTree.getHeight();
+
+        // Assert
+        assertEquals(2, x);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testGetNumberOfNodes() {
+        // Arrange
+        BinaryTree<Object> testTree = new BinaryTree<Object>(5);
+        BinaryNode<Object> L = new BinaryNode<Object>(4);
+        BinaryNode<Object> R = new BinaryNode<Object>(3);
+        testTree.getRootNode().setLeftChild(L);
+        testTree.getRootNode().setRightChild(R);
+
+        // Act
+        int x = testTree.getNumberOfNodes();
+
+        // Assert
+        assertEquals(3, x);
     }
 }
